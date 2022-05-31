1ae1e1b89d4d197ba87f1d8758d724a05f375d13
nadyrbek97 <nadyrbek97@gmail.com>
2022-05-31 18:21:28 +0600

    first commit

---
+++ backend/src/modules/db/db.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
+export class DbModule {}
