5b58a2dac49ee8f66b2e24984d805bd52d2b17af
nadyrbek97 <nadyrbek97@gmail.com>
2022-08-26 12:47:54 +0600

    remove unused comments

--- backend/src/modules/admin/admin.module.ts
+++ backend/src/modules/admin/admin.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RouterModule } from '@nestjs/core';
 import { UserEntity } from '../users/models/users.entity';
 import { UsersModule } from '../users/users.module';
 import { AdminController } from './controllers/admin.controller';
@@ -8,16 +7,7 @@ import { AdminAuthGuard } from './guards/admin-auth.guard';
 import { UsersController } from './controllers/users.controller';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([UserEntity]),
-    UsersModule,
-    // RouterModule.register([
-    //   {
-    //     path: 'users',
-    //     module: UsersModule,
-    //   },
-    // ]),
-  ],
+  imports: [TypeOrmModule.forFeature([UserEntity]), UsersModule],
   controllers: [AdminController, UsersController],
   providers: [AdminAuthGuard],
 })
