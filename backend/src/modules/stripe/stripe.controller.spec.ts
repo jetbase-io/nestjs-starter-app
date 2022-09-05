7f53c8a397c1ad5ede1d625ed596aaa0f8661dff
Ellementul <ananev.ivan.ur@ya.ru>
2022-09-05 12:05:10 +0500

    Fix stripe tests

--- backend/src/modules/stripe/stripe.controller.spec.ts
+++ backend/src/modules/stripe/stripe.controller.spec.ts
@@ -1,11 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { repositoryMockFactory } from 'src/utils/helpers/mock-repository';
+import { UserEntity } from '../users/models/users.entity';
+import { UsersService } from '../users/users.service';
 import { StripeController } from './stripe.controller';
+import { StripeService } from './stripe.service';
 
 describe('StripeController', () => {
   let controller: StripeController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: getRepositoryToken(UserEntity),
+          useFactory: repositoryMockFactory,
+        },
+        UsersService,
+        StripeService,
+      ],
       controllers: [StripeController],
     }).compile();
 
--- backend/src/modules/stripe/stripe.service.spec.ts
+++ backend/src/modules/stripe/stripe.service.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { repositoryMockFactory } from 'src/utils/helpers/mock-repository';
+import { UserEntity } from '../users/models/users.entity';
+import { UsersService } from '../users/users.service';
 import { StripeService } from './stripe.service';
 
 describe('StripeService', () => {
@@ -6,7 +10,14 @@ describe('StripeService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StripeService],
+      providers: [
+        {
+          provide: getRepositoryToken(UserEntity),
+          useFactory: repositoryMockFactory,
+        },
+        UsersService,
+        StripeService,
+      ],
     }).compile();
 
     service = module.get<StripeService>(StripeService);
