f281b764e1e5ac5de8b0f60de1505c66ae4dbbb5
Ellementul <ananev.ivan.ur@ya.ru>
2022-09-05 17:19:50 +0500

    Clear useless dependency

--- backend/src/modules/users/users.controller.spec.ts
+++ backend/src/modules/users/users.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { repositoryMockFactory } from 'src/utils/helpers/mock-repository';
 import { UserEntity } from './models/users.entity';
 import { UsersController } from './users.controller';
