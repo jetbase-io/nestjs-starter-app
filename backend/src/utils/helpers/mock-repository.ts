c51327fbf5a1390df081a1a58074b582e7913b51
Ellementul <ananev.ivan.ur@ya.ru>
2022-09-05 12:26:07 +0500

    Added new mock methods

--- backend/src/utils/helpers/mock-repository.ts
+++ backend/src/utils/helpers/mock-repository.ts
@@ -3,7 +3,10 @@ import { Repository } from 'typeorm';
 export type MockType<T> = {
   [P in keyof T]?: jest.Mock<'unknown'>;
 };
-export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(() => ({
-    findOne: jest.fn((entity) => entity),
+export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(
+  () => ({
+    save: jest.fn(),
+    findOne: jest.fn(),
+    remove: jest.fn(),
   }),
 );
