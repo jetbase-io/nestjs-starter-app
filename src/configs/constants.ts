6163e17a7401bc3bec68368ca622b48a1a6a2e48
Denis Afonkin <afonking.denis13@gmail.com>
2024-09-05 17:49:14 +0300

    feat:  file for constants created and added to main

---
+++ src/configs/constants.ts
@@ -0,0 +1,20 @@
+export const HTTPS_PATTERN = 'https://';
+
+export const DEFAULT_LOCAL_PORT = 3000;
+
+export const DEFAULT_SENTRY_DSN = '';
+
+export const API_DEFAULT_PREFIX = '/api';
+
+export const SWAGGER_PREFIX = '/docs';
+
+export const NODE_ENV = {
+  DEVELOPMENT: 'development',
+};
+
+export const NEST_CONFIGS = {
+  TITLE: 'Nest JS starter',
+  DESCRIPTION: 'Documentation for REST API',
+  VERSION: '1.0.0',
+  TAG: 'MVP Engine',
+};
